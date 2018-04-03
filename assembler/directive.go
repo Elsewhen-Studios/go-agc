8c3bcfd856912cc27efbb1e544abae34bff21086
Adam Butt <adamdbutt@gmail.com>
2018-04-03 11:56:24 -0500

    Added DEC and 2DEC directives.

--- assembler/directive.go
+++ assembler/directive.go
@@ -1,15 +1,18 @@
 package assembler
 
 import (
+	"math"
 	"strconv"
+	"strings"
 )
 
 type directiveHandler func(a *Assembler, sp *scannerPeeker, p *instructionParams) bool
 
 var directives = map[string]directiveHandler{
 	"SETLOC": setLoc,
-	//	"DEC":    dec,
-	"OCT": oct,
+	"OCT":    oct,
+	"DEC":    dec,
+	"2DEC":   dec2,
 }
 
 func setLoc(a *Assembler, sp *scannerPeeker, p *instructionParams) bool {
@@ -62,6 +65,130 @@ func oct(a *Assembler, sp *scannerPeeker, p *instructionParams) bool {
 
 	action := func(a *Assembler) bool {
 		return a.writeWordToImage(p.logger, uint16(v))
+	}
+	a.queueOperation(action)
+
+	return true
+}
+
+func tryParseDec(sp *scannerPeeker, p *instructionParams, dp bool) (uint16, uint16, bool) {
+	if err := requireOperand(sp, p); err != nil {
+		p.logger.LogError(err.Error())
+		return 0, 0, false
+	}
+
+	max := 1 << 28
+	var i int
+	if _, ok := sp.Peek(); !ok && !strings.Contains(p.operandToken, ".") {
+		//asume integer
+		v, err := strconv.ParseInt(p.operandToken, 10, 64)
+		if err != nil {
+			p.logger.LogErrorf("unable to parse %v (%v)", p.operandToken, err.Error())
+			return 0, 0, false
+		}
+
+		i = int(v)
+		if !dp {
+			i <<= 14
+		}
+
+		if i < 0 {
+			i = -i
+		}
+
+		if i >= max {
+			if dp {
+				p.logger.LogErrorf("value out of range (%v)", v)
+			} else {
+				p.logger.LogErrorf("value out of range (%v)", v)
+			}
+			return 0, 0, false
+		}
+	} else {
+		v, err := strconv.ParseFloat(p.operandToken, 64)
+		if err != nil {
+			p.logger.LogErrorf("unable to parse %v (%v)", p.operandToken, err.Error())
+			return 0, 0, false
+		}
+
+		errorOut := false
+		for {
+			s, ok := sp.Consume()
+			if !ok {
+				break
+			}
+
+			var base float64
+			if strings.HasPrefix(s, "E") {
+				base = 10
+			} else if strings.HasPrefix(s, "B") {
+				base = 2
+			} else {
+				p.logger.LogErrorf("invalid exponent base specified (%v)", 2)
+				errorOut = true
+				continue
+			}
+
+			exp, err := strconv.ParseFloat(string([]rune(s)[1:]), 64)
+			if err != nil {
+				p.logger.LogErrorf("unable to parse exponent %v (%v)", s, err.Error())
+				errorOut = true
+				continue
+			}
+
+			v *= math.Pow(base, exp)
+		}
+
+		if errorOut {
+			return 0, 0, false
+		}
+
+		v *= float64(max)
+		i = int(math.Round(v))
+		if i < 0 {
+			i = -i
+		}
+
+		if i >= max {
+			p.logger.LogErrorf("value out of range (%f)", v)
+			return 0, 0, false
+		}
+	}
+
+	var sign uint16
+	if strings.HasPrefix(p.operandToken, "-") {
+		sign = 1 << 14
+	}
+
+	mask := ((1 << 14) - 1)
+	l := uint16(i&mask) | sign
+	h := uint16((i>>14)&mask) | sign
+
+	return h, l, true
+}
+
+func dec(a *Assembler, sp *scannerPeeker, p *instructionParams) bool {
+	h, _, ok := tryParseDec(sp, p, false)
+	if !ok {
+		return false
+	}
+
+	action := func(a *Assembler) bool {
+		return a.writeWordToImage(p.logger, h)
+	}
+	a.queueOperation(action)
+
+	return true
+}
+
+func dec2(a *Assembler, sp *scannerPeeker, p *instructionParams) bool {
+	h, l, ok := tryParseDec(sp, p, true)
+	if !ok {
+		return false
+	}
+
+	action := func(a *Assembler) bool {
+		return a.writeWordToImage(p.logger, h) && a.writeWordToImage(p.logger, l)
 	}
 	a.queueOperation(action)
 
--- assembler/directive_test.go
+++ assembler/directive_test.go
@@ -1,6 +1,7 @@
 package assembler
 
 import (
+	"bufio"
 	"fmt"
 	"strings"
 	"testing"
@@ -151,5 +152,760 @@ func Test_oct_noOperand(t *testing.T) {
 	if assert.Len(t, a.Problems, 1, "problem count") {
 		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
 	}
+	assert.Len(t, a.operations, 0, "operation count")
+}
+
+const (
+	negZero = 1 << 14
+
+	max14Bit = (1 << 14) - 1
+	max28Bit = (1 << 28) - 1
+)
+
+func convertTo15BitOnes(v int) uint16 {
+	var neg bool
+	if v < 0 {
+		neg = true
+		v = -v
+	}
+
+	if v > max14Bit {
+		panic("value out of range")
+	}
+
+	if neg {
+		v |= negZero
+	}
+
+	return uint16(v)
+}
+
+func convertTo30BitOnes(v int) (h uint16, l uint16) {
+	var neg bool
+	if v < 0 {
+		neg = true
+		v = -v
+	}
+
+	if v > max28Bit {
+		panic("value out of range")
+	}
+
+	l = uint16(v & max14Bit)
+	h = uint16(v >> 14)
+
+	if neg {
+		l |= negZero
+		h |= negZero
+	}
+
+	return
+}
+
+func Test_tryParseDec_posIntegerSingle(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	value := 012345
+	line := fmt.Sprintf("%d", value)
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, convertTo15BitOnes(value), h, "high word")
+	assert.EqualValues(t, 0, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_posIntegerDouble(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	value := 01234567012
+	vh, vl := convertTo30BitOnes(value)
+	line := fmt.Sprintf("%d", value)
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, true)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, vh, h, "high word")
+	assert.EqualValues(t, vl, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negIntegerSingle(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	value := -012345
+	line := fmt.Sprintf("%d", value)
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, convertTo15BitOnes(value), h, "high word")
+	assert.EqualValues(t, negZero, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negIntegerDouble(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	value := -01234567012
+	vh, vl := convertTo30BitOnes(value)
+	line := fmt.Sprintf("%d", value)
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, true)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, vh, h, "high word")
+	assert.EqualValues(t, vl, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negIntegerSingleZero(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "-0"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, negZero, h, "high word")
+	assert.EqualValues(t, negZero, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negIntegerDoubleZero(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "-0"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, true)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, negZero, h, "high word")
+	assert.EqualValues(t, negZero, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_posIntegerSingleLarge(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := fmt.Sprintf("%d", max14Bit+1)
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_tryParseDec_posIntegerDoubleLarge(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := fmt.Sprintf("%d", max28Bit+1)
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, true)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_tryParseDec_negIntegerSingleLarge(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := fmt.Sprintf("%d", -(max14Bit + 1))
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_tryParseDec_negIntegerDoubleLarge(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := fmt.Sprintf("%d", -(max28Bit + 1))
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, true)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_tryParseDec_IntegerInvalid(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "123FooBar678"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_tryParseDec_posFloatNoExponent(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "0.75"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 030000, h, "high word")
+	assert.EqualValues(t, 0, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negFloatNoExponent(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "-0.75"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 030000|negZero, h, "high word")
+	assert.EqualValues(t, negZero, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_posFloat0Exponent(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "0.75 E0 B0"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 030000, h, "high word")
+	assert.EqualValues(t, 0, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negFloat0Exponent(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "-0.75 E0 B0"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 030000|negZero, h, "high word")
+	assert.EqualValues(t, negZero, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_posFloatDecExponent(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "75 E-2" //0.75
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 030000, h, "high word")
+	assert.EqualValues(t, 0, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negFloatDecExponent(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "-75 E-2" //-0.75
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 030000|negZero, h, "high word")
+	assert.EqualValues(t, negZero, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_posFloatBinExponent(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "3 B-2" //0.75
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 030000, h, "high word")
+	assert.EqualValues(t, 0, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negFloatBinExponent(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "-3 B-2" //-0.75
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 030000|negZero, h, "high word")
+	assert.EqualValues(t, negZero, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_posFloatDecAndBinExponent(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "300 E-2 B-2" //0.75
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 030000, h, "high word")
+	assert.EqualValues(t, 0, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negFloatDecAndBinExponent(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "-300 E-2 B-2" //-0.75
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 030000|negZero, h, "high word")
+	assert.EqualValues(t, negZero, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_posFloatTiny(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "1 B-28" //1.0
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, 0, h, "high word")
+	assert.EqualValues(t, 1, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negFloatTiny(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "-1 B-28"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, negZero, h, "high word")
+	assert.EqualValues(t, 1|negZero, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_negFloatZero(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "-0."
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	h, l, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.EqualValues(t, negZero, h, "high word")
+	assert.EqualValues(t, negZero, l, "low word")
+
+	assert.Len(t, a.Problems, 0, "problem count")
+}
+
+func Test_tryParseDec_posFloatLarge(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "50.0 E-2 B1" //1.0
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_tryParseDec_negFloatLarge(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "-1.0"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_tryParseDec_FloatInvalid(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "1.0FooBar123"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_tryParseDec_FloatExponentInvalid(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "0.75 Q2"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_tryParseDec_FloatDecExponentInvalid(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "0.75 E2Foo"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_tryParseDec_FloatBinExponentInvalid(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	p := &instructionParams{logger: pl}
+
+	line := "0.75 B2Foo"
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanWords)
+	sp := newScannerPeeker(s)
+
+	// act
+	_, _, ok := tryParseDec(sp, p, false)
+
+	// assert
+	assert.False(t, ok, "result")
+
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+}
+
+func Test_dec_valid(t *testing.T) {
+	// arrange
+	a := new(Assembler)
+
+	value := 012345
+	prog := fmt.Sprintf("SETLOC 4000\nDEC %d\nOCT 77777", value)
+
+	// act
+	ok := a.assembleReader(strings.NewReader(prog), "fake_file.asm")
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.Len(t, a.Problems, 0, "problem count")
+	assertImageValue(t, convertTo15BitOnes(value), a, 2, 0)
+	assertImageValue(t, 077777, a, 2, 1)
+}
+
+func Test_dec_noOperand(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	a.setLocation(psudoAddress(04000))
+
+	// act
+	a.parseLine(pl, "DEC ")
+
+	// assert
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+
+	assert.NotNil(t, a.location, "location")
+	assert.EqualValues(t, 04000, int(a.location), "location")
+	assert.Len(t, a.operations, 0, "operation count")
+}
+
+func Test_dec2_valid(t *testing.T) {
+	// arrange
+	a := new(Assembler)
+
+	value := 01234567012
+	vh, vl := convertTo30BitOnes(value)
+	prog := fmt.Sprintf("SETLOC 4000\n2DEC %d\nOCT 77777", value)
+
+	// act
+	ok := a.assembleReader(strings.NewReader(prog), "fake_file.asm")
+
+	// assert
+	assert.True(t, ok, "result")
+	assert.Len(t, a.Problems, 0, "problem count")
+	assertImageValue(t, vh, a, 2, 0)
+	assertImageValue(t, vl, a, 2, 1)
+	assertImageValue(t, 077777, a, 2, 2)
+}
+
+func Test_dec2_noOperand(t *testing.T) {
+	// arrange
+	a, pl := buildAssemblerLogger()
+	a.setLocation(psudoAddress(04000))
+
+	// act
+	a.parseLine(pl, "2DEC ")
+
+	// assert
+	if assert.Len(t, a.Problems, 1, "problem count") {
+		assert.EqualValues(t, ProblemKindError, a.Problems[0].Kind, "problem kind")
+	}
+
+	assert.NotNil(t, a.location, "location")
+	assert.EqualValues(t, 04000, int(a.location), "location")
 	assert.Len(t, a.operations, 0, "operation count")
 }
